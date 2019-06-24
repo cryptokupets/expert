955ef655b78f1c58121b8de05d21ea5492550a9d
Dmitry Fokin <fokind1@yandex.ru>
2019-06-24 21:00:22 +0300

    выделил диаграммы в отдельный проект

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,21 +1,14 @@
-sap.ui.define(
-  [
-    "sap/ui/core/UIComponent",
-    "ck/expert/assets/d3.min",
-    "ck/expert/assets/moment-with-locales.min"
-  ],
-  function(UIComponent) {
-    "use strict";
+sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent) {
+  "use strict";
 
-    return UIComponent.extend("ck.expert.Component", {
-      metadata: {
-        manifest: "json"
-      },
+  return UIComponent.extend("ck.expert.Component", {
+    metadata: {
+      manifest: "json"
+    },
 
-      init: function() {
-        UIComponent.prototype.init.apply(this, arguments);
-        this.getRouter().initialize();
-      }
-    });
-  }
-);
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments);
+      this.getRouter().initialize();
+    }
+  });
+});
